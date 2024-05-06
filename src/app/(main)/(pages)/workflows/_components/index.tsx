22603c8d1fc319071426f4f81d3a6c30091d329a
5ecurealf <alfielloyd12@gmail.com>
2024-05-06 14:45:28 +0100

    feat(Workflow): - minor UI change, changed the spaces between cards

--- src/app/(main)/(pages)/workflows/_components/index.tsx
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Workflows = (props: Props) => {
   return (
     <div className="relative flex flex-col gap-4">
-      <section className="flex flex-col gap-4 p-6">
+      <section className="flex flex-col m-2">
         <Workflow
           name="Test"
           description="This is a test description"
